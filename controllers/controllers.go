5f40ec423e8aa8cb0677ab566b6ba3f8870bd76f
tvianna <taiisgvianna@gmail.com>
2022-06-30 16:11:03 -0300

    feat: adding delete personalidade function

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"github.com/tvianna/api-go-rest/database"
-	"strconv"
 	"github.com/gorilla/mux"
 	"github.com/tvianna/api-go-rest/models"
 	"encoding/json"
@@ -23,10 +22,22 @@ func TodasPersonalidades(w http.ResponseWriter, r *http.Request){
 func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request){
 	vars := mux.Vars(r)
 	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
 
-	for _, personalidade := range models.Personalidades{
-		if strconv.Itoa(personalidade.Id) == id {
-			json.NewEncoder(w).Encode(personalidade)
-		}
-	}
+func CriaUmaNovaPersonalidade(w http.ResponseWriter, r *http.Request){
+	var novaPersonalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+	database.DB.Create(&novaPersonalidade)
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
+func DeletaUmaPersonalidade(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
 }
